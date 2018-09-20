3dd3030741d74a75bf9f45ac76020a240e7d2c23
abednego1979 <abednego1979@163.com>
2018-09-20 10:22:08 +0000

    patch shell

--- source/001-grbl_extender/51mcu_grbl_extender.c
+++ source/001-grbl_extender/51mcu_grbl_extender.c
@@ -6,37 +6,69 @@ translate grbl signal to 35byj-46
 /*
 ********************************
  GRBL to 35BYJ-46 Step Motor
- GRBL x step          <--> P1^0	up edge valid default
- GRBL x direct        <--> P1^1
- GRBL y step          <--> P1^2	up edge valid default
- GRBL y direct        <--> P1^3
- GRBL step enable     <--> P1^4 no used
- GRBL splindle enable <--> P1^5
- 
- STEP 1_A    <--> P2^0
- STEP 1_B    <--> P2^1
- STEP 1_C    <--> P2^2
- STEP 1_D    <--> P2^3
- STEP 2_A    <--> P2^4
- STEP 2_B    <--> P2^5
- STEP 2_C    <--> P2^6
- STEP 2_D    <--> P2^7
+x step            P0^0    up edge valid default
+x direct        P0^1    
+y step            P0^2    up edge valid default
+y direct        P0^3    
+z step            P0^4    up edge valid default
+z direct        P0^5    
+step enable        P0^6    
+            
+output signals:            
+steperx_A    P1^0        
+steperx_B    P1^1        
+steperx_C    P1^2        
+steperx_D    P1^3        
+stepery_A    P1^4        
+stepery_B    P1^5        
+stepery_C    P1^6        
+stepery_D    P1^7        
+            
+steperz_A    P2^0        
+steperz_B    P2^1        
+steperz_C    P2^2        
+steperz_D    P2^3    
 ********************************
 */
 
 #define uchar unsigned char
 #define uint unsigned int
 
-uchar step_format[8]={0x1,0x3,0x2,0x6,0x4,0xC,0x8,0x9};
+#define STEPER_MOTO_TYPE        "35BYJ46-ULN2003"
+
+#if STEPER_MOTO_TYPE=="35BYJ46-ULN2003"
+uchar step_format[8]={0x01,0x03,0x02,0x06,0x04,0x0C,0x08,0x09};
+uchar step_disable_format=0x00;
+#else
+#error "Unknown Moto type"
+#endif
 uchar x_index=0;
 uchar y_index=0;
+uchar z_index=0;
+uchar spindle_en=0;
 
-#define DIR_X_STATE ((input>>1)&0x1)
+uchar last_moving_axis=0; /*0-X, 1-Y, 2-Y*/
+
+
+/*input signal(From P1)*/
 #define STEP_X_STATE ((input>>0)&0x1)
-#define DIR_Y_STATE ((input>>3)&0x1)
-#define STEP_Y_STATE ((input>>2)&0x1)
-#define STEP_ENABLE_FLAG ((input>>4)&0x1)	/*no used*/
-#define SPLINDLE_ENABLE_FLAG ((input>>5)&0x1)
+#define STEP_Y_STATE ((input>>1)&0x1)
+#define STEP_Z_STATE ((input>>2)&0x1)
+#define DIR_X_STATE ((input>>3)&0x1)
+#define DIR_Y_STATE ((input>>4)&0x1)
+#define DIR_Z_STATE ((input>>5)&0x1)
+#define STEP_ENABLE_FLAG ((input>>6)&0x1)
+#define SPINDLE_ENABLE ((input>>7)&0x1)
+
+/*output signal*/
+/*
+MOTO X: A-B-C-D: P0.0-P0.1-P0.2-P0.3
+MOTO Y: A-B-C-D: P0.4-P0.5-P0.6-P0.7
+MOTO Z: A-B-C-D: P2.7-P2.6-P2.5-P2.4
+Spindle Enable: P2.3
+*/
+
+#define INVERT_4BIT(x) (((x<<3)&0x08)|((x<<1)&0x04)|((x>>1)&0x02)|((x>>3)&0x01))
 
 void delayShort(uchar ucDelayTime)
 {
@@ -45,25 +77,34 @@ void delayShort(uchar ucDelayTime)
     for(ui=0; ui<ucDelayTime ; ui++)
     {
         for(ucCnti=0; ucCnti<5 ; ucCnti++)
-        {    
-        }    
+        {
+        }
     }
 }
 
-void P2Output()
-{
-    P2=(step_format[y_index]<<4)|(step_format[x_index]&0x0F);
+void CtrlSignalOutput(uchar steper_en)
+{   
+    if (steper_en)
+    {
+        P0=(step_format[y_index]<<4)|(step_format[x_index]&0x0F);
+        P2=(INVERT_4BIT(step_format[z_index])<<4)|(spindle_en<<3);
+    }
+    else:
+    {
+        P0=(step_disable_format<<4)|(step_disable_format&0x0F);
+        P2=(INVERT_4BIT(step_disable_format)<<4)|(spindle_en<<3);
+    }
 }
 
 void main(void)
 {
     uchar old_step_x=0;
     uchar old_step_y=0;
+    uchar old_step_z=0;
     uchar input;
-    uchar new_step_x,new_step_y;
+    uchar new_step_x,new_step_y,new_step_z;
     
-
-    P0=0xFF;
+    P0=0x00;
     P1=0xFF;
     P2=0x00;
 
@@ -73,37 +114,58 @@ void main(void)
         delayShort(2);
         input = P1;
         
-	new_step_x=STEP_X_STATE;
-	new_step_y=STEP_Y_STATE;
+        
+    new_step_x=STEP_X_STATE;
+    new_step_y=STEP_Y_STATE;
+    new_step_z=STEP_Z_STATE;
             
-	if ((old_step_x==0)&&(new_step_x==1))
-	{
-	    if (DIR_X_STATE)
-	    {
-		x_index=(x_index+1)%8;
-	    }
-	    else
-	    {
-		x_index=(x_index+7)%8;
-	    }
-	}
-	old_step_x=new_step_x;
+    if ((old_step_x==0)&&(new_step_x==1))
+    {
+        if (DIR_X_STATE)
+        {
+        x_index=(x_index+1)%8;
+        }
+        else
+        {
+        x_index=(x_index+7)%8;
+        }
+        last_moving_axis=0;
+    }
+    old_step_x=new_step_x;
             
             
-	if ((old_step_y==0)&&(new_step_y==1))
-	{
-	    if (DIR_Y_STATE)
-	    {
-		y_index=(y_index+1)%8;
-	    }
-	    else
-	    {
-		y_index=(y_index+7)%8;
-	    }
-	}
-	old_step_y=new_step_y;
+    if ((old_step_y==0)&&(new_step_y==1))
+    {
+        if (DIR_Y_STATE)
+        {
+        y_index=(y_index+1)%8;
+        }
+        else
+        {
+        y_index=(y_index+7)%8;
+        }
+        last_moving_axis=1;
+    }
+    old_step_y=new_step_y;
+
+    if ((old_step_z==0)&&(new_step_z==1))
+    {
+        if (DIR_Z_STATE)
+        {
+        z_index=(z_index+1)%8;
+        }
+        else
+        {
+        z_index=(z_index+7)%8;
+        }
+        last_moving_axis=2;
+    }
+    old_step_z=new_step_z;
+
+    spindle_en=SPINDLE_ENABLE;/*如果Spindle_en需要反相，在这里做*/
+
             
-	P2Output();
+    CtrlSignalOutput(STEP_ENABLE_FLAG);
     }
 }
 
--- source/001-grbl_extender/readme.txt
+++ source/001-grbl_extender/readme.txt
@@ -1,19 +1,29 @@
 51mcu receive Arduino UNO R3(run grbl)'s signal and control the 35BYJ46
 
-only a few signals are accept by 51mcu:
-x step			P1^0    up edge valid default
-x direct		P1^1
-y step			P1^2    up edge valid default
-y direct		P1^3
-step enable		P1^4    no used
-splindle enable		P1^5
 
-steper1_A	P2^0
-steper1_B	P2^1
-steper1_C	P2^2
-steper1_D	P2^3
-steper2_A	P2^4
-steper2_B	P2^5
-steper2_C	P2^6
-steper2_D	P2^7
+
+
+only a few signals are accept by 51mcu:
+x step			P0^0    up edge valid default
+x direct		P0^1
+y step			P0^2    up edge valid default
+y direct		P0^3
+z step			P0^4    up edge valid default
+z direct		P0^5
+step enable		P0^6
+
+output signals:
+steperx_A	P1^0
+steperx_B	P1^1
+steperx_C	P1^2
+steperx_D	P1^3
+stepery_A	P1^4
+stepery_B	P1^5
+stepery_C	P1^6
+stepery_D	P1^7
+
+steperz_A	P2^0
+steperz_B	P2^1
+steperz_C	P2^2
+steperz_D	P2^3
 
