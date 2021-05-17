afed3363877b54b9379ed525f18e81d3096badd4
abednego1979 <abednego1979@163.com>
2021-05-17 22:42:50 +0800

    111

--- source/010-cutAudio/cutAudio.py
+++ source/010-cutAudio/cutAudio.py
@@ -3,7 +3,7 @@
 
 #功能1：播放MP3/WAV文件并计时，用户每按键盘就输出当前时间，用于记录音频分割点
 #功能2：利用分割时间，加上一个用户输入的偏移值，分割音频文件
-
+#sudo python3 cutAudio.py XXXX.mp3
 
 import os
 import sys
@@ -12,54 +12,57 @@ import keyboard
 
 def getTimeArray(audioFile):
     #play the audio
-    os.system('play %s &' % (audioFile))
+    print ("CWD:", os.getcwd())
+    CMD='play %s &' % (audioFile)
+    print ("PLAY CMD:", CMD)
+    os.system(CMD)
 
     #
     timeArray=[]
     t=time.time()
     t=int(round(t*1000))
     timeArray.append(t)
-    
+
     while True:
         if keyboard.is_pressed('a'):
             t=time.time()
             t=int(round(t*1000))
-            timeArray.append(t)
-            print (t)
-            time.sleep(0.2)
+            if t-timeArray[-1]>500:
+                print ("%d\t+%d" % (t, t-timeArray[-1]))
+                timeArray.append(t)
         if keyboard.is_pressed('s'):
             print("timeArray=", timeArray)
             break
-    
-    timeArrayDiff=[]
-    for i in range(len(timeArray)):
-        if i==0:
-            timeArrayDiff.append(0)
-            continue
-        else:
-            timeArrayDiff.append(timeArray[i]-timeArray[i-1])
-    
-    print("timeArrayDiff=", timeArrayDiff)
-    return timeArrayDiff
+
+    Base_Time=timeArray[0]
+    timeArray=[item-Base_Time for item in timeArray]
+
+    old_item=0
+    for item in timeArray:
+        print ("%d\t\t+%d" % (item, item-old_item))
+        old_item=item
+
+    return timeArray
 
 def curAudio(audioFile='', timeArrayDiff=[], timeOffset=0):
     _temp_timeArray_=[(item+timeOffset)/1000.0 for item in timeArrayDiff]
-    
+
     #以_temp_timeArray_分割目标音频文件
     new_audio_list=[]
     for i in range(len(_temp_timeArray_)-1):
+        print ("Start to cut %d" % (i+1))
         #startTime=_temp_timeArray_[i]
         #endTime=_temp_timeArray_[i+1]
         _temp0_,_temp1_=os.path.splitext(audioFile)
         _temp_new_audio_="%s_%03d%s" % (_temp0_, i+1, _temp1_)
         new_audio_list.append(_temp_new_audio_)
         os.system('sox %s %s trim %f =%f' % (audioFile, _temp_new_audio_, _temp_timeArray_[i], _temp_timeArray_[i+1]))
-    
+
     #分割以后以明显的间隔播放文件，让使用者判断是否分割正确
     for _temp_new_audio_ in new_audio_list:
         os.system('play %s' % (_temp_new_audio_))
         time.sleep(2)
-    
+
     return
 
 def main():
@@ -71,7 +74,7 @@ def main():
         assert 0
         sys.exit(1)
     
-    audioFile=argv[:realParaNum]
+    audioFile,=argv[:realParaNum]
     
     timeArrayDiff=[]
     
@@ -96,5 +99,4 @@ def main():
             break
     pass
 
-if __name__ == "__name__":
-    main()
+main()
